df8a10499bd603acd60b8fd62985783e4eb5d568
Juan <juancharlesasimoes08@gmail.com>
2024-11-01 16:12:12 -0300

    Adicionar arquivos de projeto.

---
+++ Estudante/Estudante.cs
@@ -0,0 +1,13 @@
+﻿namespace Estudante
+{
+    public class Estudante
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+
+
+
+    }
+}
