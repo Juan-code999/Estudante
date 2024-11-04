df0728523d647dcd68a0e5db50be266bcdf8c331
Juan <juancharlesasimoes08@gmail.com>
2024-11-04 16:12:35 -0300

    Parte 1

---
+++ Estudante/02-Repositorios/Data/InicializadorBd.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System.Data.SQLite;
+
+namespace Crud_Estudante._02_Repositorios.Data
+{
+    public static class InicializadorBd
+    {
+        public static void Inicializar()
+        {
+            using var connection = new SQLiteConnection("Data Source=Escola.db");
+
+            string commandoSQL = @"
+                CREATE TABLE IF NOT EXISTS Estudantes(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Nome STRING NOT NULL,
+                    Altura INTEGER NOT NULL,
+                    Peso REAL NOT NULL,
+                    Idade INTEGER NOT NULL
+                );";
+
+            connection.Execute(commandoSQL);
+        }
+    }
+}
+
+
+
+
+
+
+
